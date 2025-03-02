a011f6f95b605a9e3ba2693dff73f5b40942cc0a
Jochen Schultz <jschultz@php.net>
2025-03-02 03:04:37 +0100

    auto update bundle

--- src/Service/ChatUnifiedMessageService.php
+++ src/Service/ChatUnifiedMessageService.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace IntelligentIntern\ChatUnifiedMessageBundle\Service;
 
-use App\Contract\UnifiedMessageInterface;
+use App\Contract\UnifiedMessageServiceInterface;
 use App\Entity\ChatMessageEntry;
+use App\Entity\UnifiedMessage;
+use App\Factory\ChatCompletionServiceFactory;
 use App\Repository\InternRepository;
 use App\Repository\ChannelRepository;
 use App\Factory\ChatMemoryServiceFactory;
@@ -10,12 +13,9 @@ use App\Factory\LogServiceFactory;
 use App\Contract\LogServiceInterface;
 use App\Service\VaultService;
 use Exception;
-use IntelligentIntern\ChatUnifiedMessageServiceBundle\Payload\ChatPayload;
+use IntelligentIntern\ChatUnifiedMessageBundle\Payload\InterviewChatPayload;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
-use Symfony\Component\Messenger\Exception\ExceptionInterface;
-use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -24,7 +24,7 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use JsonSchema\Validator;
 
-class ChatUnifiedMessageService
+class ChatUnifiedMessageService implements UnifiedMessageServiceInterface
 {
     private LogServiceInterface $logger;
     private int $retryDelaySec;
@@ -40,75 +40,113 @@ class ChatUnifiedMessageService
         private readonly HubInterface $hub,
         private readonly LogServiceFactory $logServiceFactory,
         private readonly ChatMemoryServiceFactory $chatMemoryServiceFactory,
+        private readonly ChatCompletionServiceFactory $chatCompletionServiceFactory,
         private readonly InternRepository $internRepository,
         private readonly ChannelRepository $channelRepository,
-        private readonly MessageBusInterface $messageBus,
         private readonly VaultService $vaultService,
         private readonly TranslatorInterface $translator
     ) {
         $this->logger = $this->logServiceFactory->create();
-        $chatConfig = $this->vaultService->fetchSecret('secret/data/data/chat');
-        $this->retryDelaySec = $chatConfig['retryDelaySec'] ?? $this->logger->critical('retryDelaySec not found in Vault.');
+        $chatConfig = $this->vaultService->fetchSecret('secret/data/data/config');
+        $this->retryDelaySec = $chatConfig['retryDelaySec'] ?? 15;
+        $this->logger->debug('InterviewChatUnifiedMessageService constructed', [
+            'retryDelaySec' => $this->retryDelaySec,
+        ]);
     }
 
-    /**
-     * @throws Exception
-     */
-    public function handleMessage(UnifiedMessageInterface $unifiedMessage): void
+    public function supports(string $module): bool
     {
-        $chatPrompt = json_decode(
-            json_encode($unifiedMessage->getPayload()),
-            true
-        );
+        return $module === 'chat';
+    }
+
+    public function handleMessage(UnifiedMessage $message): void
+    {
+        $chatPrompt = $message->getPayload();
 
         $schemaJson = <<<'JSON'
 {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "type": "object",
-    "properties": {
-        "internId": { "type": "integer" },
-        "channelId": { "type": "integer" },
-        "chatMemoryType": { "type": "string" },
-        "prompt": { "type": "string" },
-        "completionType": { "type": "string", "enum": ["stream", "batch", "default"] },
-        "chatHistoryId": { "type": "string" },
-        "action": { "type": "string" },
-        "messageId": { "type": "string" }
-    },
-    "required": ["internId", "channelId", "chatMemoryType", "prompt", "completionType", "chatHistoryId"]
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "internId": { "type": "integer" },
+    "channel": { "type": "string" },
+    "chatMemoryType": { "type": "string" },
+    "prompt": { "type": "string" },
+    "completionType": { "type": "string", "enum": ["stream", "batch", "default"] },
+    "chatHistoryId": { "type": "string" },
+    "action": { "type": "string" },
+    "messageId": { "type": "string" }
+  },
+  "required": ["internId", "channel", "chatMemoryType", "prompt", "completionType"]
 }
 JSON;
         $schema = json_decode($schemaJson);
         $validator = new Validator();
-        $validator->validate($chatPrompt, $schema);
+        $json_decode = json_decode(json_encode($chatPrompt));
+
+        $validator->validate($json_decode, $schema);
         if (!$validator->isValid()) {
             foreach ($validator->getErrors() as $error) {
-                $this->logger->error("JSON Schema Error: " . $error['message'], $error);
+                $this->logger->error('JSON Schema Error: ' . $error['message'], $error);
             }
             throw new Exception("Unified message payload is invalid.");
         }
 
         try {
-            $intern = $this->internRepository
-                ->find($chatPrompt['internId']);
-            $channelEntity = $this->channelRepository
-                ->find($chatPrompt['channelId']);
+            $intern = $this->internRepository->find($chatPrompt['internId']);
+            if (!$intern) {
+                $this->logger->error('Intern not found - internId: ' . $chatPrompt['internId']);
+                throw new Exception('Intern not found');
+            }
+            $channelEntity = $this->channelRepository->findOneBy(['name' => $chatPrompt['channel']]);
+            if (!$channelEntity) {
+                $this->logger->error('Channel not found - channel name: ' . $chatPrompt['channel']);
+                throw new Exception('Channel not found');
+            }
+            $this->logger->debug('Building ChatMemory - type: ' . $chatPrompt['chatMemoryType']);
+
+            $systemPrompt = ChatMessageEntry::factory()
+                ->setRole('system')
+                ->setMessage($intern->getSystemPrompt());
+
+            $userPrompt = ChatMessageEntry::factory()
+                ->setRole('user')
+                ->setMessage($chatPrompt['prompt']);
+
             $chatMemory = $this->chatMemoryServiceFactory
                 ->create($chatPrompt['chatMemoryType'])
-                ->addMessageEntry(
-                    ChatMessageEntry::factory()
-                        ->setRole('system')
-                        ->setMessage($intern->getSystemPrompt())
-                )
-                ->addMessageEntry(
-                    ChatMessageEntry::factory()
-                        ->setRole('user')
-                        ->setMessage($chatPrompt['prompt'])
+                ->addMessageEntry($systemPrompt)
+                ->addMessageEntry($userPrompt)->getChatHistory();
+
+            $chatResponse = $this->chatCompletionServiceFactory
+                ->create('openai')
+                ->generateResponse('gpt4o', $chatMemory);
+
+            $payloadJson = InterviewChatPayload::factory()
+                ->createJson(
+                    'chat',
+                    $chatPrompt['chatHistoryId'] ?? '',
+                    $chatResponse->getContent()
                 );
+            $update = new Update(
+                $channelEntity->getName(),
+                $payloadJson,
+                false
+            );
+            $this->hub->publish(
+                $update,
+                ['Authorization' => 'Bearer ' . $channelEntity->getJwt()]
+            );
+            return;
+
+            $this->logger->debug('ChatMemory :' . json_encode($chatMemory));
+
             if (isset($chatPrompt['action'])) {
+                $this->logger->debug('Adding action: ' . $chatPrompt['action']);
                 $chatMemory->addAction($chatPrompt['action']);
             }
             if (isset($chatPrompt['messageId'])) {
+                $this->logger->debug('Adding messageId: ' . $chatPrompt['messageId']);
                 $chatMemory->addMessageId($chatPrompt['messageId']);
             }
             $chatMemory = $chatMemory->getChatHistory(
@@ -116,17 +154,21 @@ JSON;
                 $intern->getModel(),
                 $intern->getOptions()
             );
+
+            $payloadJson = '';
             switch ($chatPrompt['completionType']) {
                 case 'stream':
+                    $this->logger->debug('Processing stream completion');
                     $stream = $chatMemory->getCompletionStream();
                     while ($stream->isValid()) {
                         $chunk = $stream->current();
-                        $payloadJson = ChatPayload::factory()
+                        $payloadJson = InterviewChatPayload::factory()
                             ->createJson(
                                 'chat',
-                                $chatPrompt['chatHistoryId'],
+                                $chatPrompt['chatHistoryId'] ?? '',
                                 $chunk
                             );
+                        $this->logger->debug('Publishing stream chunk', ['chunk' => $chunk]);
                         $update = new Update(
                             $channelEntity->getName(),
                             $payloadJson,
@@ -140,26 +182,27 @@ JSON;
                     }
                     return;
                 case 'batch':
-                    $msg = $this->translator->trans(
-                            'batch_sent - batchId:'
-                        ) . $chatMemory->getBatchId();
-                    $payloadJson = ChatPayload::factory()
+                    $this->logger->debug('Processing batch completion');
+                    $msg = $this->translator->trans('batch_sent - batchId:') . $chatMemory->getBatchId();
+                    $payloadJson = InterviewChatPayload::factory()
                         ->createJson(
                             'chat',
-                            $chatPrompt['chatHistoryId'],
+                            $chatPrompt['chatHistoryId'] ?? '',
                             $msg
                         );
                     break;
                 case 'default':
                 default:
-                    $payloadJson = ChatPayload::factory()
+                    $this->logger->debug('Processing default completion');
+                    $payloadJson = InterviewChatPayload::factory()
                         ->createJson(
                             'chat',
-                            $chatPrompt['chatHistoryId'],
+                            $chatPrompt['chatHistoryId'] ?? '',
                             $chatMemory
                         );
                     break;
             }
+            $this->logger->debug('Publishing final update', ['payloadJson' => $payloadJson]);
             $update = new Update(
                 $channelEntity->getName(),
                 $payloadJson,
@@ -169,27 +212,15 @@ JSON;
                 $update,
                 ['Authorization' => 'Bearer ' . $channelEntity->getJwt()]
             );
-
+            $this->logger->debug('Message processed successfully');
             return;
-        } catch (Exception
-        | ClientExceptionInterface
-        | DecodingExceptionInterface
-        | RedirectionExceptionInterface
-        | ServerExceptionInterface
-        | TransportExceptionInterface $e) {
-            $this->logger->error(
-                'Error in ChatUnifiedMessageServiceStrategy',
-                ['exception' => $e]
-            );
-            $delayStamp = new DelayStamp($this->retryDelaySec * 1000);
-            try {
-                $this->messageBus->dispatch($unifiedMessage, [$delayStamp]);
-            } catch (ExceptionInterface $e) {
-                $this->logger->error(
-                    'Error dispatching unifiedMessage for retry',
-                    ['exception' => $e]
-                );
-            }
+        } catch (\Exception |
+        ClientExceptionInterface |
+        DecodingExceptionInterface |
+        RedirectionExceptionInterface |
+        ServerExceptionInterface |
+        TransportExceptionInterface $e) {
+            $this->logger->error('Error in InterviewChatUnifiedMessageService' . $e->getMessage());
         }
     }
 }
